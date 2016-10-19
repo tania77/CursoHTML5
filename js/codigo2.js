9a98624b691d8cf366d9b5e17c573a5e2c6c5ffb
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-19 17:17:56 +0100

    libreria autocompletar

--- js/codigo2.js
+++ js/codigo2.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
   $('.cookie-message').cookieBar({
     closeButton: '.close'
   });
-});
-
-var input = document.getElementById('datos');
-new Awesomplete(input, {
-  list: ["Ada", "Java", "Javascript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
+  var input = document.getElementById('datos');
+  new Awesomplete(input, {
+    list: ["Ada", "Java", "Javascript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
+  });
 });
