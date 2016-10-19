589a068c3707450c1655c5378e3e354c872fa6d0
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-19 17:16:38 +0100

    libreria de cookies

--- js/codigo2.js
+++ js/codigo2.js
@@ -3,3 +3,8 @@ $(document).ready(function() {
     closeButton: '.close'
   });
 });
+
+var input = document.getElementById('datos');
+new Awesomplete(input, {
+  list: ["Ada", "Java", "Javascript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
+});
