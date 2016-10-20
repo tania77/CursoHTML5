bc5b39571cc04ba6bbf08884eba27236ef944fcc
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-20 16:25:00 +0100

    dia 4

---
+++ js/codigo4.js
@@ -0,0 +1,10 @@
+$(document).ready({
+  var data = {
+    labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
+    series: [{
+      data: [2, 4, 6, 8, 10, 12]
+    }]
+  };
+  var options = {}
+  new chartist.Line('.chart', data, options);
+})
