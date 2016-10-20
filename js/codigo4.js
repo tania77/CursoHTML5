6ee7ca8edc3c11943bf97449a68be7e5e30defbc
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-20 18:54:01 +0100

    dia 4

--- js/codigo4.js
+++ js/codigo4.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
   $('#card').flip({
- axis: 'y', // 'x' para giro en eje X
- trigger: 'click', // 'hover': giro sobre tarjeta
- speed: 500, // Velocidad del giro
-});
+    axis: 'y',
+    trigger: 'click',
+    speed: 500
+  });
 });
