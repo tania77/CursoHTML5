5806f034a25d570ecfaf367d893498256a5552bc
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-20 19:29:06 +0100

    dia 4

--- js/codigo4.js
+++ js/codigo4.js
@@ -2,6 +2,6 @@ $(document).ready( function() {
   var PSV = PhotoSphereViewer({
     panorama: 'https://cdn.rawgit.com/mistic100/Photo-Sphere-Viewer/master/example/Bryce-Canyon-National-Park-Mark-Doliner.jpg',
     container: 'photosphere',
-    loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
+    loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif'
   });
 });
