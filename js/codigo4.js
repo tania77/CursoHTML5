fc8b317e9f2c8fd99a05d86bccf20459920dd9fd
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-20 16:59:07 +0100

    dia 4

--- js/codigo4.js
+++ js/codigo4.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-  var sample = 'Cliente->Servidor: Mensaje, C->>D: Linea con flecha hueca';
+  var sample = ['Cliente->Servidor: Mensaje', 'C->>D: Linea con flecha hueca'];
   var sample1 = 'Title: Esto es un título';
   var diagram = Diagram.parse(sample);
   diagram.drawSVG('diagram', {
