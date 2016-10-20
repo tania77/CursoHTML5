6e3b2caec18e83044c97f914a9993c0ffd2ba469
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-20 17:12:22 +0100

    dia 4

--- js/codigo4.js
+++ js/codigo4.js
@@ -1,8 +1,9 @@
 $(document).ready( function() {
-  var sample = 'Cliente->Servidor: Mensaje \n Servidor->>Cliente: Linea con flecha hueca';
-  var sample1 = 'Title: Esto es un título';
-  var diagram = Diagram.parse(sample);
-  diagram.drawSVG('diagram', {
-    theme: 'hand'
-  })
+  var sample = 'digraph g {a -> b;}';
+  var options = {
+    format: 'svg'
+  }
+  var image = Viz(sample, options);
+  var main = document.getElementById('main');
+  main.innerHTML = image;
 });
