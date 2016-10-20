806264f45446aaeca418fe1bd088677cbc016fd1
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-20 19:51:33 +0100

    dia 4

--- js/codigo4.js
+++ js/codigo4.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
 
   var PSV = PhotoSphereViewer({
-    panorama: 'https://cdn.rawgit.com/mistic100/Photo-Sphere-Viewer/master/example/Bryce-Canyon-National-Park-Mark-Doliner.jpg',
+    panorama: 'https://rawgit.com/mistic100/Photo-Sphere-Viewer/master/example/Bryce-Canyon-National-Park-Mark-Doliner.jpg',
     container: 'photosphere',
     loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif'
   });
