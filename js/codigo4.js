dc02b6943fb98e79ff7cab9741b5eaec15175e2f
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-20 16:52:13 +0100

    dia 4

--- js/codigo4.js
+++ js/codigo4.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
   var sample = 'Cliente->Servidor: Mensaje';
   var sample1 = 'Title: Esto es un título';
-  var diagram = Diagram.parse(sample1);
+  var diagram = Diagram.parse(sample);
   diagram.drawSVG('diagram', {
     theme: 'hand'
   })
