23727e76f733e34c68aeec59604a594273b62e87
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-20 17:18:38 +0100

    dia 4

--- js/codigo4.js
+++ js/codigo4.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-  var sample = 'digraph g {a -> b [label="1"]; b -> c [label="0"]; c -> a [label="1"]; a -> d [label="0"];}';
+  var sample = 'digraph g {a -> b [label="1"]; b -> c [label="0"]; c -> a [label="1"]; a -> d [label="0"]; d [style=bold]}';
   var options = {
     format: 'svg'
   }
