eee9835a868f91bb1e6d59e54db1bf207c88fc66
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-20 16:47:07 +0100

    dia 4

--- js/codigo4.js
+++ js/codigo4.js
@@ -1,13 +1,7 @@
 $(document).ready( function() {
-  var data = {
-    /*labels: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6'],
-    series: [{
-      data: [2, 4, 6, 8, 10, 12]
-    }] */
-    series: [60, 10, 30, 60]
-  };
-  var options = {
-    total: 100
-  }
-  new Chartist.Pie('.chart', data, options);
+  var sample = 'A->B: Mensaje';
+  var diagram = Diagram.parse(sample);
+  diagram.drawSVG('diagram', {
+    theme: 'hand'
+  })
 });
